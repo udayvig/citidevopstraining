71e2e4f0a78a46e33f5239cf37362b4caba6faeb
tempyolo <55802929+tempyolo@users.noreply.github.com>
2020-10-17 12:56:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++){
 		System.out.println("Hello, World! (from newfeature Jenkins :))");
+		System.out.println("Hello, World! (from newfeature Jenkins :))");
 	} 
     }
 }
